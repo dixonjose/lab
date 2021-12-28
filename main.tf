b7900929cc4530aebc55dba0a629c4e1e4fe038d
Dixon Jose <dixonjose.cloud@gmail.com>
2021-12-28 22:58:26 +0100

    Dixon changes

---
+++ main.tf
@@ -0,0 +1,43 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  ip_address_type     = "public"
+  dns_name_label      = "example-weatherapi"
+  os_type             = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "dixonjose/lab:1"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+  
